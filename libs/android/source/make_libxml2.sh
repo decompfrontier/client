a3dfe6bd9c8263121c04f0bac80496ae758a8bf4
Arves100 <4847635+arves100@users.noreply.github.com>
2025-03-27 18:34:52 +0100

    Make a proper script to build libxml2

--- libs/android/source/make_libxml2.sh
+++ libs/android/source/make_libxml2.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-NDK_ROOT=$ANDROID_SDK/ndk/21.0.6113669/
+ANDROID_SDK_2=$(cygpath -u "$ANDROID_SDK")
+NDK_ROOT=$ANDROID_SDK_2/ndk/21.0.6113669
 API_LEVEL=21
-SYSROOT=$NDK_ROOT/platforms/android-$API_LEVEL/arch-arm64
+SYSROOT=$NDK_ROOT/sysroot
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 OSNAME=linux-x86_64
@@ -12,10 +13,10 @@ OSNAME=windows-x86_64
 fi
 
 export CC="$NDK_ROOT/toolchains/llvm/prebuilt/$OSNAME/bin/aarch64-linux-android$API_LEVEL-clang"
-export CFLAGS="--sysroot=$SYSROOT"
-export LDFLAGS="--sysroot=$SYSROOT"
+
+rm -fr libxml2
+mkdir libxml2
+tar -xzf libxml2-a03fa1623ff0721f5e99fbaa26cfaa8c1388868a.tar.gz -C libxml2
 
 cd libxml2
-./autogen.sh --host=aarch64-linux-gnueabi --enable-static --disable-shared
-make libxml2.la $*
-cd ..
+./autogen.sh --host=aarch64-linux-gnueabi --enable-static --disable-shared --without-iconv --without-lzma
